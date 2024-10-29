a9800d2cb433360c64f7ffb90743d617c48d5313
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-10-29 15:59:12 +0500

    Update add_student_page.dart

--- pages/add_student_page.dart
+++ pages/add_student_page.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: avoid_print, avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
 
 class AddStudentPage extends StatefulWidget {
   const AddStudentPage({super.key});
@@ -7,11 +10,145 @@ class AddStudentPage extends StatefulWidget {
 }
 
 class _AddStudentPageState extends State<AddStudentPage> {
+  final _formkey = GlobalKey<FormState>();
+
+  var name = "";
+  var email = "";
+  var password = "";
+  final nameController = TextEditingController();
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  clearText() {
+    nameController.clear();
+    emailController.clear();
+    passwordController.clear();
+  }
+
+  addUser() {
+    print("User Added");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Add Student Page',
-      style: TextStyle(fontSize: 50.0),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add New Student"),
+      ),
+      body: Form(
+        key: _formkey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30),
+          child: ListView(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(),
+                child: TextFormField(
+                  autofocus: false,
+                  decoration: const InputDecoration(
+                      labelText: 'Name:',
+                      labelStyle: TextStyle(fontSize: 20),
+                      border: OutlineInputBorder(),
+                      errorStyle:
+                          TextStyle(color: Colors.redAccent, fontSize: 15)),
+                  controller: nameController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Name';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(),
+                child: TextFormField(
+                  autofocus: false,
+                  decoration: const InputDecoration(
+                      labelText: 'Email:',
+                      labelStyle: TextStyle(fontSize: 20),
+                      border: OutlineInputBorder(),
+                      errorStyle:
+                          TextStyle(color: Colors.redAccent, fontSize: 15)),
+                  controller: emailController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Email';
+                    } else if (!value.contains("@")) {
+                      return 'Please Enter Valid Email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10.0),
+                child: TextFormField(
+                  autofocus: false,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      labelText: 'Password:',
+                      labelStyle: TextStyle(fontSize: 20.0),
+                      border: OutlineInputBorder(),
+                      errorStyle:
+                          TextStyle(color: Colors.redAccent, fontSize: 15)),
+                  controller: passwordController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          if (_formkey.currentState!.validate()) {
+                            setState(() {
+                              name = nameController.text;
+                              email = emailController.text;
+                              password = passwordController.text;
+                              addUser();
+                              clearText();
+                            });
+                          }
+                        },
+                        child: const Text(
+                          "Register",
+                          style: TextStyle(fontSize: 18.0),
+                        )),
+                    ElevatedButton(
+                      onPressed: () => {clearText()},
+                      // ignore: sort_child_properties_last
+                      child: const Text(
+                        'Reset',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blueGrey),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
