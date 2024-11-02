da9b44a1f2a894d3fdedaf9b057c8d121db66588
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-11-03 00:19:05 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:crud/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyCOTQPzpsk-LpP3fCCeuZaBHR4osLWI4DQ",
+          authDomain: "flutter-crud-5c1bd.firebaseapp.com",
+          projectId: "flutter-crud-5c1bd",
+          storageBucket: "flutter-crud-5c1bd.firebasestorage.app",
+          messagingSenderId: "54945484609",
+          appId: "1:54945484609:web:98f2276d46556b3f9cdbc0",
+          measurementId: "G-HF54JETB7Y"));
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,15 +34,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -73,10 +74,9 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
+        shape: const CircleBorder(),
         child: const Icon(Icons.add),
       ),
-
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
